c5c37c3e063a047a24c23669ed5f27568a20b42c
yang <he_dyang@163.com>
2026-07-14 19:58:28 +0800

    现在这个模型有了完整的训练流程

--- Desktop/study_vesion/torch_buildmodel.py
+++ Desktop/study_vesion/torch_buildmodel.py
@@ -3,6 +3,24 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torchvision.transforms import v2
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+)
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+)
+# batch_size=64: 每次吐出 64 条数据
+# shuffle=True: 打乱数据顺序
+training_dataloader = DataLoader(dataset=training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(dataset=test_data, batch_size=64, shuffle=False)
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 #print(f"Using {device} device")       
@@ -33,3 +51,54 @@ pred_probab = nn.Softmax(dim=1)(logits)
 y_pred = pred_probab.argmax(1)
 #print(f"Predicted class: {y_pred}")
 
+#设置超参数
+learning_rate = 1e-3
+batch_size = 64
+epochs = 5
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+   
+    model.train()                               #这一步开启了训练模式，启用 Batch Normalization 和 Dropout（随机丢弃神经元）
+    for batch, (X, y) in enumerate(dataloader): #X：当前批次的特征数据（[64, 1, 28, 28] 的矩阵张量），y：当前批次的真实标签（是什么东西）
+        X, y = X.to(device), y.to(device)       #注意：将数据移动到cuda上,和模型位置保持一致
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        #反向传播和优化
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test_loop(dataloader, model, loss_fn):
+
+    model.eval()        #开启评估模式
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()        #.item()：从张量中提取浮点数
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()#先判断再转换为浮点数，最后求和
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+loss_fn = nn.CrossEntropyLoss()         #这个损失函数包含了Softmax(n.LogSoftmax)和负对数似然(nn.NLLLoss)的功能
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)   #尝试不同优化器
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loop(training_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+print("Done!")
